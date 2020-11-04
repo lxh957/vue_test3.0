5776e1d16b819846ad4f14d9c5eaccb636dd9896
andy <lxh10182437>
2020-11-04 11:14:29 +0800

    【】

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from "./router"
 Vue.config.productionTip = false
 
+//公共事件Bus
+const bus = new Vue()
+Vue.prototype.$bus = bus
+
+
+import store from "./vuex/store"
+Vue.config.silent = true
 new Vue({
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app')
